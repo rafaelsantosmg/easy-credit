3c88f788b081461f8dbfe134ef0cdf6f13379186
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2026-06-07 16:29:23 -0300

    feat: implementação das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'simulacao', pathMatch: 'full' },
+  {
+    path: 'simulacao',
+    loadComponent: () =>
+      import('./pages/simulation/simulation.component').then((m) => m.SimulationComponent),
+  },
+  {
+    path: 'analise',
+    loadComponent: () =>
+      import('./pages/analysis/analysis.component').then((m) => m.AnalysisComponent),
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () =>
+      import('./pages/registration/registration.component').then((m) => m.RegistrationComponent),
+  },
+  {
+    path: 'formalizacao',
+    loadComponent: () =>
+      import('./pages/formalization/formalization.component').then((m) => m.FormalizationComponent),
+  },
+  { path: '**', redirectTo: 'simulacao' },
+];
